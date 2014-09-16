79ffdff11f18bf2cc9d5be1402396ed97684374c
Kun Shao <kunshao@umich.edu>
2014-09-16 17:03:20 -0400

    load_data first commit

---
+++ php/sql/load_data.sql
@@ -0,0 +1,24 @@
+--- The below statements have been executed locally -----
+
+INSERT INTO Users
+VALUES ('sportslover', 'Paul', 'Walker', 'test_password', 'sportslover@hotmail.com');
+
+INSERT INTO Album
+VALUES (1000, 'I love sports', '2014-09-16 15:27:13', '2014-09-16 15:27:13', 'sportslover');
+
+INSERT INTO Album
+VALUES (1001, 'I love football', '2014-09-16 15:28:14', '2014-09-16 15:28:14', 'sportslover');
+
+INSERT INTO Photo
+VALUES(2000, 'test_url', 'PEG', '2012-01-05 15:15:00');
+
+INSERT INTO Contain
+VALUES(1000, 2000, 'test_caption', 1);
+
+--- The above statements have been executed locally -----
+
+INSERT INTO Users
+VALUES ('traveler', 'Rebecca', 'Travolta', 'rebt@explorer.org');
+
+INSERT INTO Users
+VALUES ('spacejunkie', 'Bob', 'Spacey', 'bspace@spacejunkies.net');
