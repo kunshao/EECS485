e15efa01c73fae7fee84ed0b0b497a401bf2a9c3
Kun Shao <kunshao@umich.edu>
2014-09-17 20:30:32 -0400

    fixed hardcoded album id
    see sql/create_tables.sql for albumid AUTOINCREMENT

--- php/html/add.php
+++ php/html/add.php
@@ -3,11 +3,10 @@ echo "hehe";
 include 'db.php';
 if(isset($_POST["title"])){
     $t= $_POST['title'];
-    $sql = "INSERT INTO Album (albumid, title, created, lastupdated, username) 
-              VALUES (:albumid, :title, :created, :lastupdated, :username)";
+    $sql = "INSERT INTO Album (title, created, lastupdated, username) 
+              VALUES (:title, :created, :lastupdated, :username)";
     $stmt = $pdo->prepare($sql);
     $stmt->execute(array(
-        ':albumid' => 1004,
         ':title' => $t,
         ':created' => "2014-09-16 15:27:13",
         ':lastupdated' => "2014-09-16 15:27:13",
--- php/html/templates/templates_c/bdd8ea3b45d33386fcd7b64d5e18e1d772927432.file.albums.tpl.php
+++ php/html/templates/templates_c/bdd8ea3b45d33386fcd7b64d5e18e1d772927432.file.albums.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.14, created on 2014-09-17 05:00:38
+<?php /* Smarty version Smarty-3.1.14, created on 2014-09-18 01:48:35
          compiled from "/Developer/Workspace/EECS485/php/html/templates/templates/albums.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:148273686554179cf94a3813-42692476%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'bdd8ea3b45d33386fcd7b64d5e18e1d772927432' => 
     array (
       0 => '/Developer/Workspace/EECS485/php/html/templates/templates/albums.tpl',
-      1 => 1410901418,
+      1 => 1410985279,
       2 => 'file',
     ),
     '94c5279065c052d6b4fb57b23b562087691fc151' => 
@@ -47,6 +47,26 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 <p class="important">
  Welcome!
 </p>
+<?php if (isset($_smarty_tpl->tpl_vars['album_title']->value)){?>
+<?php if (count($_smarty_tpl->tpl_vars['album_title']->value)==0){?>
+<p>Empty Albums for <?php echo $_smarty_tpl->tpl_vars['user_id']->value;?>
+</p>
+<?php }else{ ?>
+<?php  $_smarty_tpl->tpl_vars['item'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['item']->_loop = false;
+ $_from = $_smarty_tpl->tpl_vars['album_title']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['item']->key => $_smarty_tpl->tpl_vars['item']->value){
+$_smarty_tpl->tpl_vars['item']->_loop = true;
+?>
+  	<a href="/album?id=<?php echo $_smarty_tpl->tpl_vars['item']->key;?>
+"><?php echo $_smarty_tpl->tpl_vars['item']->value;?>
+</a>
+  	<br>
+<?php } ?>
+<br>
+<a href="/albums/edit?username=<?php echo $_smarty_tpl->tpl_vars['user_id']->value;?>
+">Edit the albums</a>
+<?php }?>
+<?php }?>
 
   </div>
   <script type="text/javascript" src="/static/js/main.js"></script>
